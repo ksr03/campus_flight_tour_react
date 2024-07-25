665738a184852e96c49c869f20dbb9cfe181609e
ksr03 <K_Haruka03@outlook.jp>
2024-07-25 19:01:17 +0900

    マップ画像の差し替えと現在位置のキャリブレーション

--- src/utils/checkCollision.ts
+++ src/utils/checkCollision.ts
@@ -1,16 +1,19 @@
 import * as THREE from 'three'
 
-// 地面の高さ
-const GROUND_HEIGHT = 0.5
+// 高さの最小値
+const MIN_HEIGHT = 0.5
+
+// マップの大きさ
+const MAP_SIZE = 9
 
 /**
  * 地面・建物との衝突判定を行う関数
  */
 function checkCollision(cameraPosition: [number, number, number], velocity: THREE.Vector3): [number, number, number] {
   const newPosition: [number, number, number] = [
-    Math.max(-5, Math.min(5, cameraPosition[0] + velocity.x)),
-    Math.max(GROUND_HEIGHT, cameraPosition[1] + velocity.y),
-    Math.max(-5, Math.min(5, cameraPosition[2] + velocity.z))
+    Math.max(-MAP_SIZE / 2, Math.min(MAP_SIZE / 2, cameraPosition[0] + velocity.x)),
+    Math.max(MIN_HEIGHT, cameraPosition[1] + velocity.y),
+    Math.max(-MAP_SIZE / 2, Math.min(MAP_SIZE / 2, cameraPosition[2] + velocity.z))
   ];
 
   // 衝突判定ロジック
